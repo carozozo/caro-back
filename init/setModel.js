0d1815a9635759d059303e5a08b9a784d1be9087
Caro Huang <carozozo@yahoo.com.tw>
2017-09-20 17:28:27 +0800

    把 app/_model 底下的子項目往上移

--- init/setModel.js
+++ init/setModel.js
@@ -2,5 +2,7 @@ ck.requireDir(`_library`)
 ck.requireDir(`_support`)
 ck.boot.on(`runStacks`, () => {
   ck.requireDir(`${ck.APP_PATH}/_fakeData`)
-  ck.requireDir(`${ck.APP_PATH}/_model`)
+  ck.requireDir(`${ck.APP_PATH}/_schema`)
+  ck.requireDir(`${ck.APP_PATH}/_data`)
+  ck.requireDir(`${ck.APP_PATH}/_service`)
 })
