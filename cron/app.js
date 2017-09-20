ef225f409b97c133a51249b719c5a7d036867541
Caro Huang <carozozo@yahoo.com.tw>
2017-09-20 09:51:34 +0800

    更新 cron 寫入 log 格式

--- cron/app.js
+++ cron/app.js
@@ -9,18 +9,20 @@ require(`boot/connectDb`)
 
 ck.boot.on(`runStacks`, async () => {
   if (!ck.IS_FIRST_PROCESS) return
+  ck.createLog(`cron`)   // 建立一個叫 cron 的 log
   const cron = ck.cron
-
-  const log = (name, expression, description) => {
-    ck.info(`-${name}- Cron Job [${expression}] ${description}`)
+  const log = (name, expression, description, result) => {
+    if (result) {
+      return ck.log.cron(`-${name}- Cron Job [${expression}] ${description}`, result)
+    }
+    return ck.log.cron(`-${name}- Cron Job [${expression}] ${description}`)
   }
 
   ck.requireDir(`${__dirname}/task`, {skip: true})
   cron.befTask((description, expression) => {
     log(`執行`, expression, description)
   }).aftTask((result, description, expression) => {
-    log(`完畢`, expression, description)
-    ck.log(`[${expression}] ${description} result=`, result)
+    log(`完畢`, expression, description,result)
   }).onTasking((description, expression) => {
     log(`跳過`, expression, description)
   }).run((description, expression) => {
