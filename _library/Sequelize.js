a7833d5c384fbb9d80922d681aae3d7ac364e16f
Caro Huang <carozozo@yahoo.com.tw>
2017-10-07 17:07:40 +0800

    更新 node 環境到 v8.6.0

--- _library/Sequelize.js
+++ _library/Sequelize.js
@@ -46,7 +46,44 @@ class Sequelize {
 
   connectDb (host, port, database, username, pwd, opt = {}) {
     return new Promise((resolve, reject) => {
+      const Op = this.Sequelize.Op
       opt = _.merge(opt, {host, port})
+      opt.operatorsAliases = {
+        $eq: Op.eq,
+        $ne: Op.ne,
+        $gte: Op.gte,
+        $gt: Op.gt,
+        $lte: Op.lte,
+        $lt: Op.lt,
+        $not: Op.not,
+        $in: Op.in,
+        $notIn: Op.notIn,
+        $is: Op.is,
+        $like: Op.like,
+        $notLike: Op.notLike,
+        $iLike: Op.iLike,
+        $notILike: Op.notILike,
+        $regexp: Op.regexp,
+        $notRegexp: Op.notRegexp,
+        $iRegexp: Op.iRegexp,
+        $notIRegexp: Op.notIRegexp,
+        $between: Op.between,
+        $notBetween: Op.notBetween,
+        $overlap: Op.overlap,
+        $contains: Op.contains,
+        $contained: Op.contained,
+        $adjacent: Op.adjacent,
+        $strictLeft: Op.strictLeft,
+        $strictRight: Op.strictRight,
+        $noExtendRight: Op.noExtendRight,
+        $noExtendLeft: Op.noExtendLeft,
+        $and: Op.and,
+        $or: Op.or,
+        $any: Op.any,
+        $all: Op.all,
+        $values: Op.values,
+        $col: Op.col
+      }
       this._connection = new this.Sequelize(database, username, pwd, opt)
       this._connection.authenticate().then((err) => {
         if (err) return reject(err)
