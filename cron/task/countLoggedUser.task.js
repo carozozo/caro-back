a02d85611da4255084324c68708f5901c68a2a99
Caro Huang <carozozo@yahoo.com.tw>
2017-12-03 01:11:41 +0800

    移除 countLoggedUser 及調整 cron 範例

--- cron/task/countLoggedUser.task.js
+++
@@ -1,8 +0,0 @@
-ck.cron.regJob(`每分鐘寫入登入人數`, `*/1 * * * *`, async () => {
-  // 故意延遲, 測試下次觸發 task 會不會執行
-  await new Promise((r) => {
-    setTimeout(() => r(), 61000)
-  })
-  const count = await ck.tokenMod.count()
-  return await ck.countLoggedUserMod.create({count})
-})
---
+++ cron/task/oneMinute.task.js
@@ -0,0 +1,8 @@
+ck.cron.regJob(`每分鐘執行一次`, `*/1 * * * *`, async () => {
+  ck.log(`內容開始`)
+  // 故意延遲, 測試下次觸發 task 會不會執行
+  await new Promise((r) => {
+    setTimeout(() => r(), 61000)
+  })
+  ck.log(`內容結束`)
+})
