a917dee1d817986f3e9c11f00719850ec14d519e
Caro Huang <carozozo@yahoo.com.tw>
2018-04-24 15:30:46 +0800

    更新 .sample 範例

--- test/spec/_model/cacheDb/tokenMod.spec.js
+++ test/spec/_model/cacheDb/tokenMod.spec.js
@@ -1,18 +1,10 @@
 describe(`tokenMod`, () => {
-  let tokenMod
-  let tokenFak
-
-  before(() => {
-    tokenMod = ck.tokenMod
-    tokenFak = ck.tokenFak
-  })
-
   describe(`extendExpiredTime`, () => {
     it(`should found null when set days 0`, async () => {
       const where = {username: `extendExpiredTimeUser`}
-      await tokenFak.fake(where) // 建立假資料用來測試
-      await tokenMod.extendExpiredTime(where, 0)
-      const found = await tokenMod.findOne(where)
+      await ck.tokenFak.fake(where) // 建立假資料用來測試
+      await ck.tokenMod.extendExpiredTime(where, 0)
+      const found = await ck.tokenMod.findOne(where)
       assert.isNull(found)
     })
   })
--- test/spec/_model/mianDb/profileMod.spec.js
+++ test/spec/_model/mianDb/profileMod.spec.js
@@ -1,36 +1,28 @@
-describe(`profileMod`, () => {
-  let profileMod
-  let profileFak
-
-  before(() => {
-    profileMod = ck.profileMod
-    profileFak = ck.profileFak
-  })
-
+describe(`ck.profileMod`, () => {
   describe(`createProfile`, () => {
     it(``, async () => {
       const $user = await ck.userFak.fake()
       const role = $user.role
       const username = $user.username
-      const _profile = profileFak.genCreate({user_username: username})
-      const $profile = await profileMod.createProfile(role, _profile)
+      const _profile = ck.profileFak.genCreate({user_username: username})
+      const $profile = await ck.profileMod.createProfile(role, _profile)
       assert.equal($profile.name, _profile.name)
     })
     it(`should got error if no relative to user`, async () => {
       const username = `userNotExists`
-      const _profile = profileFak.genCreate({user_username: username})
+      const _profile = ck.profileFak.genCreate({user_username: username})
       await assert.shouldGotErr(async () => {
-        await profileMod.createProfile(`admin`, _profile)
+        await ck.profileMod.createProfile(`admin`, _profile)
       })
     })
     it(`should got error if no email when relative user role is customer/stuff/manager`, async () => {
-      const rolesNeedEmail = profileMod.rolesNeedEmail
+      const rolesNeedEmail = ck.profileMod.rolesNeedEmail
       for (const role of rolesNeedEmail) {
         const $user = await ck.userFak.fake({role})
         const username = $user.username
         await assert.shouldGotErr(async () => {
-          const _profile = profileFak.genCreate({user_username: username, email: undefined})
-          await profileMod.createProfile(role, _profile)
+          const _profile = ck.profileFak.genCreate({user_username: username, email: undefined})
+          await ck.profileMod.createProfile(role, _profile)
         })
       }
     })
