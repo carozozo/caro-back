fc953fadd683ff48ed992b45210892dd5524d494
Caro Huang <carozozo@yahoo.com.tw>
2018-04-27 09:40:32 +0800

    修正 app.js bug

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ global._ = require(`caro`)
 global.ck = new CaroBack()
 
 if (cluster.isMaster && process.env.CLUSTER === `true`) {
-  ck.logger.log(`master #${process.pid} is running`)
+  console.log(`master #${process.pid} is running`)
   for (let i = 0; i < Math.max(cupLength, 1); ++i) {
-    ck.logger.log(`Start fork worker [${i}]`)
+    console.log(`Start fork worker [${i}]`)
     process.env.WORKER_INDEX = i // 寫入 process.env 讓 fork 出來的 worker process 使用
     cluster.fork()
   }
 
   cluster.on(`online`, (worker) => {
-    ck.logger.log(`Worker #${worker.process.pid} online`)
+    console.log(`Worker #${worker.process.pid} online`)
   })
   cluster.on(`exit`, (worker) => {
-    ck.logger.log(`Worker #${worker.process.pid} exit`)
+    console.log(`Worker #${worker.process.pid} exit`)
   })
 } else {
   require(`init`)
