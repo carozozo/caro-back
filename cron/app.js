0d09bdd47b0616709b4b2cc96cafe3bbf091515d
Caro Huang <carozozo@yahoo.com.tw>
2018-04-27 09:30:55 +0800

    修正 cron bug

--- cron/app.js
+++ cron/app.js
@@ -8,21 +8,19 @@ require(`init/setModule`)
 require(`boot/connectDb`)
 
 ck.boot.on(`runStacks`, async () => {
-  if (!ck.IS_FIRST_PROCESS) return
-  ck.createLog(`cron`)   // 建立一個叫 cron 的 log
   const cron = ck.cron
   const log = (name, expression, description, result) => {
     if (result) {
-      return ck.log.cron(`-${name}- Cron Job [${expression}] ${description}`, result)
+      return ck.logger.log(`-${name}- Cron Job [${expression}] ${description}`, result)
     }
-    return ck.log.cron(`-${name}- Cron Job [${expression}] ${description}`)
+    return ck.logger.log(`-${name}- Cron Job [${expression}] ${description}`)
   }
 
   ck.requireDir(`${__dirname}/task`, {skip: true})
   cron.befTask((description, expression) => {
     log(`執行`, expression, description)
   }).aftTask((result, description, expression) => {
-    log(`完畢`, expression, description,result)
+    log(`完畢`, expression, description, result)
   }).onTasking((description, expression) => {
     log(`跳過`, expression, description)
   }).run((description, expression) => {
