9bd2356c20cfd54f583e1ed1d3463267a19437ab
Caro Huang <carozozo@yahoo.com.tw>
2018-04-27 11:15:50 +0800

    app 更名為 module

--- init/setModule.js
+++ init/setModule.js
@@ -1,8 +1,1 @@
-ck.requireDir(`app/_library`)
-ck.requireDir(`app/_support`)
-ck.boot.on(`runStacks`, () => {
-  ck.requireDir(`app/_fakeData`)
-  ck.requireDir(`app/_schema`)
-  ck.requireDir(`app/_model`)
-  ck.requireDir(`app/_controller`)
-})
+ck.requireDir(`module`)
