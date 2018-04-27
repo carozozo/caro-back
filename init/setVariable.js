6ca16a5745d51afc87038c52d02addb429a329fe
Caro Huang <carozozo@yahoo.com.tw>
2018-04-27 10:29:16 +0800

    移除 ck.WORKER_INDEX

--- init/setVariable.js
+++ init/setVariable.js
@@ -1,5 +1,4 @@
 const env = process.env.NODE_ENV
 
-ck.WORKER_INDEX = parseInt(process.env.WORKER_INDEX || 0, 10)
-ck.IS_FIRST_PROCESS = ck.WORKER_INDEX === 0
+ck.IS_FIRST_PROCESS = parseInt(process.env.WORKER_INDEX || 0, 10) === 0
 ck.CAN_DROP_DB = ck.IS_FIRST_PROCESS && (env === `dev` || env === `beta`)
