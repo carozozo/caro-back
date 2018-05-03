fe05aa884b2bb1d9136f0de2ecbb394df99d81f0
Caro Huang <carozozo@yahoo.com.tw>
2018-05-03 17:48:27 +0800

    更新 sample.js

--- sample.js
+++ sample.js
@@ -1,39 +1,38 @@
 /*
- `TARGET=xxx FOLDER=xxx npm run sample`
+ `TARGET=xxx  npm run sample`
  找出所有 .sample 檔案, 並複製成 .js 檔案
 
- `TARGET=xxx FOLDER=xxx npm run sample:model`
+ `TARGET=xxx  npm run sample:model`
  找出 model 相關的 .sample 檔案, 並複製成 .js 檔案
 
- `TARGET=xxx FOLDER=xxx npm run sample:route`
+ `TARGET=xxx  npm run sample:route`
  找出 route 相關的 .sample 檔案, 並複製成 .js 檔案
 
- `TARGET=xxx FOLDER=xxx npm run sample.delete`
+ `TARGET=xxx  npm run sample.delete`
  找出所有 .sample 對應的檔案, 並移除對應的 .js 檔案
 
- `TARGET=xxx FOLDER=xxx npm run sample.delete:model`
+ `TARGET=xxx  npm run sample.delete:model`
  找出 model 相關的 .sample 檔案, 並移除對應的 .js 檔案
 
- `TARGET=xxx FOLDER=xxx npm run sample.delete:route`
+ `TARGET=xxx  npm run sample.delete:route`
  找出 route 相關的 .sample 檔案, 並移除對應的 .js 檔案
 */
 const _ = require(`caro`)
 const fs = require(`fs`)
 const path = require(`path`)
 const targetName = process.env.TARGET
-const folder = process.env.FOLDER
 const sampleType = process.env.SAMPLE_TYPE
 const isDelete = process.env.IS_DELETE
 
 if (!targetName) {
   console.error(`
     請輸入
-    'TARGET=xxx FOLDER=xxx npm run sample'              建立相關檔案
-    'TARGET=xxx FOLDER=xxx npm run sample:model'        建立 model 相關檔案
-    'TARGET=xxx FOLDER=xxx npm run sample:route'        建立 route 相關檔案
-    'TARGET=xxx FOLDER=xxx npm run sample.delete'       移除相關檔案
-    'TARGET=xxx FOLDER=xxx npm run sample.delete:model' 移除 model 相關檔案
-    'TARGET=xxx FOLDER=xxx npm run sample.delete:route' 移除 route 相關檔案
+    'TARGET=xxx npm run sample'              建立相關檔案
+    'TARGET=xxx npm run sample:model'        建立 model 相關檔案
+    'TARGET=xxx npm run sample:route'        建立 route 相關檔案
+    'TARGET=xxx npm run sample.delete'       移除相關檔案
+    'TARGET=xxx npm run sample.delete:model' 移除 model 相關檔案
+    'TARGET=xxx npm run sample.delete:route' 移除 route 相關檔案
   `)
   process.exit()
 }
@@ -44,9 +43,7 @@ const getFilePathFromSample = (samplePath) => {
     .replace(`__`, _.upperFirst(targetName)) // e.g. __Ctr.sample => UserCtr.sample
     .replace(`_`, targetName) // e.g. _Ctr.sample => userCtr.sample
     .replace(`.sample`, `.js`)
-  const folderPath = path.join(dirPath, folder)
-  if (!fs.existsSync(folderPath)) fs.mkdirSync(folderPath)
-  return path.join(folderPath, fileName)
+  return path.join(dirPath, fileName)
 }
 const createFile = (samplePath) => {
   let data = fs.readFileSync(samplePath, `utf8`)
@@ -82,16 +79,17 @@ const getSampleFile = (fileOrDir) => {
 }
 
 if (!sampleType) {
-  getSampleFile(`app/route`)
   getSampleFile(`app/module`)
+  getSampleFile(`app/route`)
+  getSampleFile(`app/model`)
   getSampleFile(`test/spec`)
   return
 }
 
 if (sampleType === `route`) {
-  getSampleFile(`app/route`)
   getSampleFile(`app/module/controller`)
   getSampleFile(`app/module/service`)
+  getSampleFile(`app/route`)
   getSampleFile(`test/spec/route`)
   getSampleFile(`test/spec/controller`)
   getSampleFile(`test/spec/service`)
