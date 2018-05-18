df17f3871a8dd116408b9182dfd77e19c19c6c42
Caro Huang <carozozo@yahoo.com.tw>
2018-05-18 17:33:29 +0800

    更新, 如果寫入的 response 是 Error, 要能明確的寫入資訊

--- app/module/support/res.js
+++ app/module/support/res.js
@@ -12,17 +12,32 @@ class Req {
     // 如果要 log 的狀態是 suc, 但不要寫入回傳的結果
     if (status === `suc` && this.ifPathInSkipLogResponse(path)) return `[skip]`
 
+    // 簡化 array
     const maxLength = 10
-    if (!_.isArray(response) || response.length <= maxLength) return response
+    if (_.isArray(response) && response.length > maxLength) {
+      const responseDataLength = response.length
+      const ret = []
 
-    const responseDataLength = response.length
-    const ret = []
-
-    for (let i = 0; i < maxLength; i++) {
-      ret.push(response[i])
+      for (let i = 0; i < maxLength; i++) {
+        ret.push(response[i])
+      }
+      ret.push(`... and ${responseDataLength - maxLength} more`)
+      return ret
     }
-    ret.push(`... and ${responseDataLength - maxLength} more`)
-    return ret
+
+    if (_.isError(response)) {
+      const ret = []
+      const stacks = response.stack.split(`\n`)
+      const stackLength = stacks.length
+      const maxStackLength = _.min([20, stackLength])
+
+      for (let i = 0; i < maxStackLength; i++) {
+        ret.push(stacks[i]) // 每一行執行序
+      }
+      return ret
+    }
+
+    return response
   }
 
   convertResponseData (data) {
